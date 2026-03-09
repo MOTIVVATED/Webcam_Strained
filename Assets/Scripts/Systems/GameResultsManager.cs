f9b0786ad0f82fef4590698f6d81d30fe6024b5c
ft_fst <lebedevfo69@gmail.com>
2026-03-09 13:56:08 +0100

    I added Save/Load logic in GameResultsManager

--- Assets/Scripts/Systems/GameResultsManager.cs
+++ Assets/Scripts/Systems/GameResultsManager.cs
@@ -1,16 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+[Serializable]
+public class GameResultsList
+{ 
+    public List<GameResult> results = new List<GameResult>();
+}
+
+public class GameResultsManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameResultsManager Instance { get; private set; }
+
+    private GameResultsList _data = new GameResultsList();
+    private string _savePath;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        _savePath = Path.Combine(Application.persistentDataPath, "game_results.json");
+        Load();
+    }
+    public void SaveResult(int score, int itemsCaught, int itemsDodged, float timeSurvived)
+    {
+        var result = new GameResult(score, itemsCaught, itemsDodged, timeSurvived);
+        _data.results.Add(result);
+        Save();
+        Debug.Log($"Result saved: Score ={score}, Date={result.dateTime}");
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<GameResult> GetAllResults() => _data.results;
+
+    public GameResult GetBestScore()
     {
-        
+        if (_data.results.Count == 0) return null;
+        GameResult best = _data.results[0];
+        foreach (var r in _data.results)
+            if (r.score > best.score) best = r;
+        return best;
+    }
+
+    public void ClearAll()
+    {
+        _data.results.Clear();
+        Save();
+    }
+
+    private void Save()
+    {
+        string json = JsonUtility.ToJson(_data, prettyPrint: true);
+        File.WriteAllText(_savePath, json);
+    }
+
+    private void Load()
+    {
+        if (File.Exists(_savePath))
+        {
+            string json = File.ReadAllText(_savePath);
+            _data = JsonUtility.FromJson<GameResultsList>(json) ?? new GameResultsList();
+        }
     }
 }
