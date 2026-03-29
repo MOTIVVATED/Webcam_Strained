142bd36b25c5a4e31ec17d99d44d685d1838c41a
ft_fst <lebedevfo69@gmail.com>
2026-03-29 12:15:46 +0200

    Went from a singleton controlling one hardcoded panel to a clean spawner + self-contained instances pattern The code got simpler in the process, not more complex

--- Assets/Scripts/UI/DistractionOverlay.cs
+++ Assets/Scripts/UI/DistractionOverlay.cs
@@ -1,32 +1,17 @@
 using UnityEngine;
-using UnityEngine.UI;
 public class DistractionOverlay : MonoBehaviour
 {
 	public static DistractionOverlay Instance;
 
-	[SerializeField] private GameObject overlayPanel;
-	[SerializeField] private Image overlayImage;
+	[SerializeField] private GameObject popupPrefab;
 	[SerializeField] private Transform parentCanvas;
-
-	[SerializeField] private float minX = -2f;
-	[SerializeField] private float maxX = 2f;
-	[SerializeField] private float minY = -2f;
-	[SerializeField] private float maxY = 2f;
 
 	private void Awake()
 	{
 		Instance = this;
-		overlayPanel.SetActive(false);
 	}
 	public void Show()
 	{
-		float randomX = Random.Range(minX, maxX);
-		float randomY = Random.Range(minY, maxY);
-		overlayImage.rectTransform.anchoredPosition = new Vector2(randomX, randomY);
-		overlayPanel.SetActive(true);
-	}
-	public void OnPlayerTap()
-	{
-		overlayPanel.SetActive(false);
+		Instantiate(popupPrefab, parentCanvas);
 	}
 }
