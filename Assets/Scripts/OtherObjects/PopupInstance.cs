e240aaabe2c435c523718bb131f561ca4f99dd04
ft_fst <lebedevfo69@gmail.com>
2026-03-29 14:33:51 +0200

    I added a text with random names in Resources, so now, each new window popups with a random name from the .txt file.

--- Assets/Scripts/OtherObjects/PopupInstance.cs
+++ Assets/Scripts/OtherObjects/PopupInstance.cs
@@ -1,9 +1,14 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PopupInstance : MonoBehaviour
 {
 	[SerializeField] private Button closeButton;
+	[SerializeField] private Image popupImage;
+	[SerializeField] private Text popupText;
+
+	[SerializeField] private Sprite[] possibleSprites;
 
 	[SerializeField] private float minX = -200f;
 	[SerializeField] private float maxX = 200f;
@@ -14,8 +19,18 @@ public class PopupInstance : MonoBehaviour
 	{
 		float randomX = Random.Range(minX, maxX);
 		float randomY = Random.Range(minY, maxY);
-
 		GetComponent<RectTransform>().anchoredPosition = new Vector2(randomX, randomY);
+
+		if (possibleSprites.Length > 0 )
+			popupImage.sprite = possibleSprites[Random.Range(0, possibleSprites.Length)];
+
+		TextAsset textFile = Resources.Load<TextAsset>("popupTexts");
+		if ( textFile != null)
+		{
+			string[] lines = textFile.text.Split('\n');
+			if (lines.Length > 0)
+				popupText.text = lines[Random.Range(0, lines.Length)].Trim();
+		}
 
 		closeButton.onClick.AddListener(Close);
 	}
