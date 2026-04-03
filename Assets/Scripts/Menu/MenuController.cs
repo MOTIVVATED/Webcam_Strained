f1191032c09e3fb70815845112bc8f1b87d7e02a
ft_fst <lebedevfo69@gmail.com>
2026-04-03 14:42:35 +0200

    I reordered files in ASEPRITE folder, made a folder for each root object, that contains animations, animators, json file and sprites for each child object. I made a button in mane menue that sets ModelSelectionPanel active. Now, I'm going to work on the ModelSelectionPanel.

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -3,20 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField] private string gameSceneName = "Game";
+	[SerializeField] private string gameSceneName = "Game";
+	[SerializeField] private GameObject ModelSelectionPanel;
 
-    public void Start()
-    {
-        Time.timeScale = 0.4f;
-    }
-    public void Play()
-    {
-        Time.timeScale = 0.4f;
-        SceneManager.LoadScene(gameSceneName);
-    }
-    public void Quit()
-    {
-        Application.Quit();
-        Debug.Log("Quit");
-    }
+	public void Start()
+	{
+		Time.timeScale = 0.4f;
+	}
+	public void Play()
+	{
+		Time.timeScale = 0.4f;
+		SceneManager.LoadScene(gameSceneName);
+	}
+	public void SelectModel()
+	{
+		ModelSelectionPanel.SetActive(true);
+	}
+	public void Quit()
+	{
+		Application.Quit();
+		Debug.Log("Quit");
+	}
 }
