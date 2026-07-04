47702ba9492c5502b2505ed5c892b7483fce4a5e
ft_fst <lebedevfo69@gmail.com>
2026-07-04 17:04:56 +0500

    I can't say leveling panel is beautiful, but it works and looks finished.

--- Assets/DevJournal.cs
+++ Assets/DevJournal.cs
@@ -22,6 +22,7 @@
 * 1) I fixed bug with unhighlited rank. Now it works properly. The problem was that I tried to call
 * Refresh() in Start() or in Open(), but I needed to call it in both.
 * 2) I set up leveling menu visual style. Now it looks much better.
+* 3) I can't say leveling panel is beautiful, but it works and looks finished.
 * 
 * 
 * 03.07.2026
