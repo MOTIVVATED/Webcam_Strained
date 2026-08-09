f456fde7b2805484eae284bed46db364946009fb
ft_fst <lebedevfo69@gmail.com>
2026-08-09 17:27:49 +0500

    have no time :(

---
+++ Assets/Scripts/UI/HowToPlayPanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HowToPlayPanel : MonoBehaviour
+{
+	[SerializeField] private GameObject panelRoot;
+
+	public void Open()
+	{
+		panelRoot.SetActive(true);
+	}
+
+	public void Close()
+	{
+		panelRoot.SetActive(false);
+	}
+}
